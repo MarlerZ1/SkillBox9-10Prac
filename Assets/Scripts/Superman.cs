5c4a495a41c7815ced858901b33b2f64b7747e26
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-03-31 14:02:26 +0300

    finish 9.1 superman scene

--- Assets/Scripts/Superman.cs
+++ Assets/Scripts/Superman.cs
@@ -6,15 +6,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class Superman : MonoBehaviour
 {
-    [SerializeField] private damageForce;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float damageForce;
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.layer == 7)
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            print("Bad");
+            if (rb)
+            {
+                print("Damaged");
+                rb.AddForce((collision.transform.position - gameObject.transform.position).normalized * damageForce, ForceMode.Impulse);
+            }
+        }
     }
 }
