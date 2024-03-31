c02de6cc10ae0ed20fd01da3be9832253f69d8e2
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-03-31 13:48:34 +0300

    Add physical layers

---
+++ Assets/Scripts/Superman.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Superman : MonoBehaviour
+{
+    [SerializeField] private damageForce;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
