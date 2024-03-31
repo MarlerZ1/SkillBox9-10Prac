b712c5b3fa7a6ba2123ecf2b37ff3c6117fadf88
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-03-31 18:38:53 +0300

    Pintball

---
+++ Assets/Scripts/PinballForce.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinballForce : MonoBehaviour
+{
+    [SerializeField] private float force;
+    [SerializeField] private Transform SprintL;
+    [SerializeField] private Transform SprintR;
+
+    public void PushSphere() 
+    {
+
+    }
+}
