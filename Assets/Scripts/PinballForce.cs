261cb2e9994f29a893550835a894c47ef8a52f7a
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2024-04-11 11:57:33 +0300

    ADD 10.2

--- Assets/Scripts/PinballForce.cs
+++ Assets/Scripts/PinballForce.cs
@@ -2,14 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
 public class FinballForce : MonoBehaviour
 {
-    [SerializeField] private float force;
-    [SerializeField] private Transform SprintL;
-    [SerializeField] private Transform SprintR;
+    [SerializeField] private Transform preparingPosition;
+    [SerializeField] private Transform launchingPosition;
 
-    public void PushSphere() 
+    [SerializeField] private float launchSpeed = 10f;
+    [SerializeField] private float returnSpeed = 2f;
+
+    [SerializeField] private float returnDelay = 2f;
+    [SerializeField] private float waitTime = 1f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+        StartCoroutine(WorkSpring());
+    }
+
+    private IEnumerator WorkSpring()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(Launch());
+            yield return StartCoroutine(Return());
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+
+    private IEnumerator Launch()
+    {
+        while (Vector3.Distance(transform.position, launchingPosition.position) > 0.1f)
+        {
+            _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, launchingPosition.position, launchSpeed * Time.deltaTime));
+            yield return null;
+        }
+    }
+
+    private IEnumerator Return()
+    {
+        yield return new WaitForSeconds(returnDelay);
+
+        while (Vector3.Distance(transform.position, preparingPosition.position) > 0.1f)
+        {
+            _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, preparingPosition.position, returnSpeed * Time.deltaTime));
+            yield return null;
+        }
     }
 }
+
